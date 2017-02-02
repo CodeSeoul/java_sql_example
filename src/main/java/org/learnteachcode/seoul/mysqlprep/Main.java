021db39d4d56883b6d7e70b778a7d8c66dd760b2
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2017-02-02 03:36:03 -0800

    Select example works. Pulled out into functions.

--- src/main/java/org/learnteachcode/seoul/mysqlprep/Main.java
+++ src/main/java/org/learnteachcode/seoul/mysqlprep/Main.java
@@ -4,8 +4,13 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
+        selectExample();
+    }
+
+    public static void insertExample() {
+        Connection connection = null;
         try {
-            Connection connection = DriverManager.getConnection(
+            connection = DriverManager.getConnection(
                     "jdbc:mariadb://localhost:3306/classprep",
                     "java_class",
                     "bestclass"
@@ -17,6 +22,41 @@ public class Main {
             connection.commit();
         } catch(SQLException e) {
             System.out.println("Hit exception doing database stuff: " + e);
+        } finally {
+            try {
+                connection.close();
+            } catch(SQLException e1) {
+                System.out.println("Failed to close DB connection: " + e1);
+            }
+        }
+    }
+
+    public static void selectExample() {
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(
+                    "jdbc:mariadb://localhost:3306/classprep",
+                    "java_class",
+                    "bestclass"
+            );
+            String insertQuery = "SELECT * FROM examples;";
+            PreparedStatement statement = connection.prepareStatement(insertQuery);
+            statement.execute();
+            ResultSet results = statement.getResultSet();
+            while(results.next()) {
+                String name = results.getString("name");
+                System.out.println("name: " + name);
+            }
+            connection.commit();
+        } catch(SQLException e) {
+            System.out.println("Hit exception doing database stuff: " + e);
+        } finally {
+            try {
+                connection.close();
+            } catch(SQLException e1) {
+                System.out.println("Failed to close DB connection: " + e1);
+            }
         }
     }
 }
